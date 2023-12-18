796d75c04664d094cdb2a4a7c1bef3a845eb8f86
Pak <pakapongm@gmail.com>
2023-12-17 20:10:55 -0800

    added a forgotpassword and used nodemailer to send email to the user, currently supports gmail

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 // signing up will enter information into database
 router.post('/signup', (req, res) => {
@@ -70,7 +71,47 @@ router.post('/login', (req, res) => {
             return res.status(500).json(err);
         }
     })
+});
+var transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+// forgot password will send password to user email if user exists in database 
+router.post('/forgotpassword', (req, res) => {
+    const user = req.body;
+    query = "select email, password from user where email =?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(200).json({ message: "Password sent to email" });
+            }
+            else {
+                // email information is taken from body and sent to user email
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'Password Reset',
+                    text: 'Your email is' + results[0].email + 'Your password is ' + results[0].password
+                };
+                // send email to user with password information  
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error)
+                    }
+                    else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+                return res.status(200).json({ message: "Password sent to email" });
+            }
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
 })
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const http = require('http');
 const app = require('./index')
 
 const server = http.createServer();
-server.listen(process.env.PORT);
+server.listen(process.env.PORT)
+server.on('listening', () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
 
