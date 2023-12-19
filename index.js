41b47b973596d021c49dddf110d334e3ce835767
Pak <pakapongm@gmail.com>
2023-12-18 19:49:24 -0800

    update index.use middleware to include category

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/user');
+const categoryRoute = require('./routes/category');
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // user route
-app.use('/user',userRoute)
+app.use('/user',userRoute);
+app.use('/category',categoryRoute);
 
 module.exports = app;
